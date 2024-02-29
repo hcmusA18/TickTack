2fa1dea3361c4433e83701560c76b85f0e18490d
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-02-29 15:27:54 +0700

    [TIC-2} feat: Update UI video posting page

--- frontend/src/pages/SavePostPage.tsx
+++ frontend/src/pages/SavePostPage.tsx
@@ -1,20 +1,29 @@
-import { View, Text, TouchableOpacity, TextInput, Image, ActivityIndicator } from 'react-native'
-import { Feather } from '@expo/vector-icons'
+import { View, Text, TouchableOpacity, TextInput, Image, Modal } from 'react-native'
+import { Feather, AntDesign } from '@expo/vector-icons'
 import { AppStackScreenProps } from '../navigators'
 import React, { FC, useState, useRef } from 'react'
+import { RadioButton } from 'react-native-paper'
 
 interface SavePostPageProps extends AppStackScreenProps<'SavePost'> {}
 
 export const SavePostPage: FC<SavePostPageProps> = (props) => {
-  console.log('SavePostPageProps:', props)
+  //   console.log('SavePostPageProps:', props)
   const { navigation } = props
 
   const [textInput, setTextInput] = useState('')
   const textInputRef = useRef<TextInput>(null)
+  const [privacy, setPrivacy] = useState('private')
+  const [privacyButton, setPrivacyButton] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false)
 
   const addHashTag = () => {
     setTextInput((prev) => prev + '#')
     textInputRef.current?.focus()
+  }
+
+  const handlePrivacyChange = (value: string) => {
+    setPrivacy(value)
+    // setModalVisible(false)
   }
 
   return (
@@ -30,6 +39,7 @@ export const SavePostPage: FC<SavePostPageProps> = (props) => {
             value={textInput}
             onChangeText={setTextInput}
           />
+
           <TouchableOpacity style={styles.hashtagButton} onPress={addHashTag}>
             <Feather name="hash" size={13} color="black" />
             <Text style={{ color: 'black', fontSize: 12, fontWeight: 'bold' }}>Hashtags</Text>
@@ -37,7 +47,21 @@ export const SavePostPage: FC<SavePostPageProps> = (props) => {
         </View>
         <Image style={styles.mediaPreview} source={{ uri: props.route?.params?.source }} />
       </View>
+
+      <View style={styles.settingContainer}>
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <View style={styles.privacyContainer}>
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+              <Feather name="lock" size={24} />
+              <Text style={{ marginLeft: 10 }}>Privacy</Text>
+            </View>
+            <AntDesign name="right" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
+
       <View style={styles.spacer} />
+
       <View style={styles.buttonsContainer}>
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.cancelButton}>
           <Feather name="x" size={24} color="black" />
@@ -49,6 +73,29 @@ export const SavePostPage: FC<SavePostPageProps> = (props) => {
           <Text style={styles.postButtonText}>Post</Text>
         </TouchableOpacity>
       </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false)
+        }}
+        style={styles.modalViewContainer}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={{ fontSize: 17, marginBottom: 10, fontWeight: 'bold' }}>Who can see your post?</Text>
+            <RadioButton.Group onValueChange={handlePrivacyChange} value={privacy}>
+              <RadioButton.Item label="Public" value="public" />
+              <RadioButton.Item label="Private" value="private" />
+              <RadioButton.Item label="Friends" value="friends" />
+            </RadioButton.Group>
+            <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.saveButton}>
+              <Text>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -140,5 +187,35 @@ const styles = {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 16
+  },
+  modalViewContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+  settingContainer: {
+    flex: 1
+  },
+  privacyContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20
+  },
+  saveButton: {
+    alignItems: 'center',
+    marginTop: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    paddingVertical: 10,
+    borderRadius: 10
   }
 }
