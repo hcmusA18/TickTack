0b6055734ae652ab3f56181c64d29edad88a33ea
Tăng Tường Thoại <thoaitang24603@gmail.com>
2024-03-07 00:46:59 +0700

    feat: login page init

--- frontend/src/pages/LoginPages.tsx
+++ frontend/src/pages/LoginPages.tsx
@@ -1,20 +1,16 @@
 import React, { FC, useEffect, useState } from 'react'
-import { ViewStyle, StyleSheet } from 'react-native'
-import { Text, Button, TextInput } from 'react-native-paper'
+import { StyleSheet, View, ScrollView } from 'react-native'
+import { Text, Button } from 'react-native-paper'
 import { AppStackScreenProps } from '../navigators'
 import { Screen } from '../components'
 import { spacing } from '../theme'
 import { useAppDispatch, useAppSelector } from 'libs/redux'
 import { setAuth } from 'libs/redux/sliceAuth'
+// import { setYear } from 'date-fns'
+// import materialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+// import { EventSubscriptionVendor } from 'react-native'
 
 interface LoginPageProps extends AppStackScreenProps<'Login'> {}
-
-const styles = StyleSheet.create({
-  contentContainer: {
-    paddingVertical: spacing.xxl,
-    paddingHorizontal: spacing.lg
-  } as ViewStyle
-})
 
 export const LoginPage: FC<LoginPageProps> = (props) => {
   const { navigation } = props
@@ -55,27 +51,149 @@ export const LoginPage: FC<LoginPageProps> = (props) => {
     setPassword('')
   }
 
+  // return (
+  //   <Screen preset="auto" contentContainerStyle={styles.contentContainer} safeAreaEdges={['top', 'bottom']}>
+  //     <Text variant="headlineLarge">Login</Text>
+  //     <TextInput
+  //       value={inputEmail}
+  //       label="Email"
+  //       placeholder="Type your email"
+  //       onChangeText={(nextValue) => setInputEmail(nextValue)}
+  //     />
+  //     <TextInput
+  //       value={password}
+  //       label="Password"
+  //       placeholder="Type your password"
+  //       left={<TextInput.Icon icon="alert-circle-outline" />}
+  //       right={<TextInput.Icon icon={isPasswordVisible ? 'eye' : 'eye-off'} onPress={togglePasswordVisible} />}
+  //       secureTextEntry={!isPasswordVisible}
+  //       onChangeText={(nextValue) => setPassword(nextValue)}
+  //     />
+  //     <Button onPress={login} mode="outlined">
+  //       Login
+  //     </Button>
+  //   </Screen>
+  // )
+
   return (
-    <Screen preset="auto" contentContainerStyle={styles.contentContainer} safeAreaEdges={['top', 'bottom']}>
-      <Text variant="headlineLarge">Login</Text>
-      <TextInput
-        value={inputEmail}
-        label="Email"
-        placeholder="Type your email"
-        onChangeText={(nextValue) => setInputEmail(nextValue)}
-      />
-      <TextInput
-        value={password}
-        label="Password"
-        placeholder="Type your password"
-        left={<TextInput.Icon icon="alert-circle-outline" />}
-        right={<TextInput.Icon icon={isPasswordVisible ? 'eye' : 'eye-off'} onPress={togglePasswordVisible} />}
-        secureTextEntry={!isPasswordVisible}
-        onChangeText={(nextValue) => setPassword(nextValue)}
-      />
-      <Button onPress={login} mode="outlined">
-        Login
-      </Button>
+    // <SafeAreaView style={styles.container}>
+    //   {/* Top bar */}
+    //   <View>
+    //     <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    //       Press me
+    //     </Button>
+    //     <Text>Log in to Tiktok</Text>
+    //     <Button></Button>
+    //   </View>
+
+    //   {/* Login Methods List  */}
+
+    //   {/* Policy bar */}
+
+    //   {/* Don't have account? Sign up */}
+    // </SafeAreaView>
+
+    <Screen preset="fixed" safeAreaEdges={['top', 'bottom']} contentContainerStyle={styles.container}>
+      <View style={styles.container}>
+        {/* Top bar */}
+        <View style={styles.topBar}>
+          <Button icon="help-circle-outline" onPress={() => console.log('Help Button Pressed')}>
+            <Text style={styles.buttonText}>Help</Text>
+          </Button>
+          <Text style={styles.buttonText}>Log in to Tiktok</Text>
+          <Button icon="close" onPress={() => console.log('Cancel Button Pressed')}>
+            <Text style={styles.buttonText}>Close</Text>
+          </Button>
+        </View>
+
+        <View>
+          {/* Content */}
+          <ScrollView>
+            <Text style={styles.login_text}>Sign up for Tiktok</Text>
+            <Text>Create a profile, follow other accounts, make your own videos, and more.</Text>
+
+            <View>
+              <Button icon="account-outline">
+                <Text>Sign up</Text>
+              </Button>
+              <Text>Use phone or email</Text>
+            </View>
+
+            <View>
+              <Button icon="account-outline">
+                <Text>Sign up</Text>
+              </Button>
+              <Text>Continue with Facebook</Text>
+            </View>
+
+            <View>
+              <Button icon="account-outline">
+                <Text>Sign up</Text>
+              </Button>
+              <Text>Continue with Apple</Text>
+            </View>
+
+            <View>
+              <Button icon="account-outline">
+                <Text>Sign up</Text>
+              </Button>
+              <Text>Continue with Google</Text>
+            </View>
+
+            <View>
+              <Button icon="account-outline">
+                <Text>Sign up</Text>
+              </Button>
+              <Text>Continue with Line</Text>
+            </View>
+
+            <View>
+              <Button icon="account-outline">
+                <Text>Sign up</Text>
+              </Button>
+              <Text>Continue with KakaoTalk</Text>
+            </View>
+          </ScrollView>
+          {/* Policy */}
+          <View>
+            <Text>
+              By continuing with an account located in Vietnam, you agree to out Terms of Service and acknowledge that
+              you have read our Privacy Policy
+            </Text>
+          </View>
+          {/* Bottom bar */}
+          <View>
+            <View>
+              <Text>Don't have an account?</Text>
+              <Button>
+                <Text>Sign up</Text>
+              </Button>
+            </View>
+          </View>
+        </View>
+      </View>
     </Screen>
   )
 }
+
+const styles = StyleSheet.create({
+  // contentContainer: {
+  //   paddingVertical: spacing.xxl,
+  //   paddingHorizontal: spacing.lg
+  // },
+  container: {
+    flex: 1
+  },
+  topBar: {
+    height: spacing.xxxl, // Adjust height as needed
+    flexDirection: 'row',
+    justifyContent: 'space-between', // Center elements horizontally
+    alignItems: 'center' // Center elements vertically
+  },
+  buttonText: {
+    marginLeft: 10 // Adjust spacing between icon and text as needed
+  },
+  login_text: {
+    fontSize: 24
+  }
+})
