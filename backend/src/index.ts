0642cee97f9df991ee1d941015270244d45045c7
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-03-07 18:42:00 +0700

    [TIC-5] feat: Use pg for postgres database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,16 +3,19 @@ import cors from "cors";
 import express from "express";
 import userRouter from "./routes/user.route";
 import authMiddleware from "./middlewares/auth.middleware";
-import * as admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+import pool from "./models/db";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// PostgreSQL connection
+pool.connect((err: Error) => {
+  if (err) {
+    console.error("Database connection error", err.stack);
+  } else {
+    console.log("Connected to database");
+  }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -22,10 +25,6 @@ app.use(authMiddleware.authenticate);
 
 // user router
 app.use("/", userRouter);
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
 
