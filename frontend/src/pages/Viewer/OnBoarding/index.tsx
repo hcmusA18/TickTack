2d3ceb536f62914773b24edff68dc441cf135aa9
GiaThinn <trangiathinh53@gmail.com>
2024-04-24 17:15:15 +0700

    [TIC-11] fix: fix sonar p3

--- frontend/src/pages/Viewer/OnBoarding/index.tsx
+++ frontend/src/pages/Viewer/OnBoarding/index.tsx
@@ -12,7 +12,6 @@ export const OnboardingPage: FC<OnboardingPageProps> = (props) => {
   const navigation = props.navigation
 
   const getInterests = () => {
-    // TODO: get interests from the server
     const result = [
       {
         interestGroup: 'Entertainment & Culture',
@@ -119,71 +118,5 @@ export const OnboardingPage: FC<OnboardingPageProps> = (props) => {
     </SafeAreaView>
   )
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1
-//   },
-//   TopBarContainer: {
-//     flex: 1,
-//     backgroundColor: colors.ttt.pjWhite,
-//     flexDirection: 'row',
-//     justifyContent: 'flex-end',
-//     maxHeight: 50
-//   },
-//   buttonSkip: {
-//     justifyContent: 'center',
-//     marginRight: '5%'
-//   },
-//   skipText: {
-//     fontSize: 15,
-//     color: colors.ttt.ink100,
-//     textAlign: 'center'
-//   },
-//   headerText: {
-//     fontSize: 40,
-//     fontWeight: 'bold',
-//     marginVertical: 8
-//   },
-//   detailText: {
-//     fontSize: 21,
-//     color: colors.ttt.ink200,
-//     marginVertical: 8
-//   },
-//   // Content
-//   ContentContainer: {
-//     flex: 8,
-//     backgroundColor: colors.ttt.pjWhite,
-//     width: '90%',
-//     marginHorizontal: '5%'
-//   },
-//   // Bottom
-//   bottomContainer: {
-//     flex: 1,
-//     backgroundColor: colors.ttt.pjWhite
-//   },
-//   nextButton: {
-//     width: '90%',
-//     marginTop: 8,
-//     marginHorizontal: '5%',
-//     backgroundColor: colors.ttt.UIGrey,
-//     borderRadius: 4,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     marginBottom: 24,
-//     height: '65%'
-//   },
-//   selectedNextButton: {
-//     backgroundColor: colors.ttt.UIRed
-//   },
-//   nextButtonText: {
-//     color: colors.ttt.ink100,
-//     fontSize: 16,
-//     fontWeight: '600'
-//   },
-//   NextBtnPressedText: {
-//     color: colors.ttt.pjWhite
-//   }
-// })
 
 export default OnboardingPage
